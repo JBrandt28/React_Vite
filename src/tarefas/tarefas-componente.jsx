c368d042e6a250d21f53897f81c1015bdc58ae10
Jonathan <141784238+JBrandt28@users.noreply.github.com>
2024-05-06 21:41:05 -0300

    Add files via upload

--- src/tarefas/tarefas-componente.jsx
+++ src/tarefas/tarefas-componente.jsx
@@ -18,35 +18,74 @@ function Tarefas() {
     //caso não utilizar o this, utilizar o aeroFunction
     // const adicionarTarefa = () => {
     // }
-    function marcarComoFinalizado(index) {
-        const novasTarefas = [...listaTarefas];
-        novasTarefas[index].finalizado = true;
-        setListaTarefas(novasTarefas);
+    // function marcarComoFinalizado(index) {
+    //     const novasTarefas = [...listaTarefas];
+    //     novasTarefas[index].finalizado = true;
+    //     setListaTarefas(novasTarefas);
+    // }
+
+    function atualizarTarefa(tarefaAtual){
+        tarefaAtual.finalizado = !tarefaAtual.finalizado;
+        setListaTarefas(listaTarefas.slice())
+        // if(tarefaAtual.finalizado) {
+        //     tarefaAtual.finalizado = false;
+        // } else {
+        //     tarefaAtual.finalizado = true;
+        // }
     }
 
+    function pegaEstilo(tarefaAtual) {
+        if (tarefaAtual.finalizado) {
+            return 'line-through';
+        }
+        return 'none';
+    }
 
     return (
         <div>
-        <button onClick={adicionarTarefa}>Cadastrar</button>
-        <br />
+            <button onClick={adicionarTarefa}>Cadastrar</button>
+            <br />
             <div>
-                {listaTarefas.map((tarefaAtual, index) => (
-                    <div
-                        key={index}
-                        style={{
-                            margin: '10px',
-                            color: tarefaAtual.finalizado ? 'dark' : 'greenyellow',
-                            backgroundColor: 'gray',
-                            textDecoration: tarefaAtual.finalizado ? 'line-through' : 'none'
-                        }}
-                        onClick={() => marcarComoFinalizado(index)}
-                    >
-                        {tarefaAtual.descricao}
-                    </div>
-                ))}
+                {
+                    listaTarefas.map(tarefaAtual => {
+                        return <div style={
+                            {
+
+                                margin: '10px',
+                                color: 'greenyellow',
+                                backgroundColor: 'gray',
+                                textDecoration: pegaEstilo(tarefaAtual)
+                                //textDecoration: tarefaAtual.finalizado ? 'line-through' : 'none'
+                            }
+                        } onClick= {() => atualizarTarefa(tarefaAtual)}>{tarefaAtual.descricao}</div>
+                    })
+                }
             </div>
         </div>
     )
+    
+
+    // return (
+    //     <div>
+    //     <button onClick={adicionarTarefa}>Cadastrar</button>
+    //     <br />
+    //         <div>
+    //             {listaTarefas.map((tarefaAtual, index) => (
+    //                 <div
+    //                     key={index}
+    //                     style={{
+    //                         margin: '10px',
+    //                         color: tarefaAtual.finalizado ? 'dark' : 'greenyellow',
+    //                         backgroundColor: 'gray',
+    //                         textDecoration: tarefaAtual.finalizado ? 'line-through' : 'none'
+    //                             }
+    //                     }onClick={() => marcarComoFinalizado(index)}
+    //                 >{tarefaAtual.descricao}
+    //                 </div>
+    //             ))}
+    //         </div>
+    //     </div>
+    // )
 }
 
 export default Tarefas;
