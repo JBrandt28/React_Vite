3f1b84a287d3b8b681b302cd32c10823a26c1a78
Jonathan <141784238+JBrandt28@users.noreply.github.com>
2024-05-07 20:42:56 -0300

    Add files via upload

--- src/tarefas/tarefas-componente.jsx
+++ src/tarefas/tarefas-componente.jsx
@@ -1,91 +1,96 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 function Tarefas() {
     const [listaTarefas, setListaTarefas] = useState([]);
-    
-    //caso utilizar o this, fazer uso do function
-    function adicionarTarefa() {
-        listaTarefas.push(
-            {
-                descricao: 'Tarefa',
-                finalizado: false
-            }
-        );
-        setListaTarefas(listaTarefas.slice());
-        //console.log('Tarefas: ', listaTarefas);
-        console.log('Cadastrado');
-    }
-    //caso não utilizar o this, utilizar o aeroFunction
-    // const adicionarTarefa = () => {
-    // }
-    // function marcarComoFinalizado(index) {
-    //     const novasTarefas = [...listaTarefas];
-    //     novasTarefas[index].finalizado = true;
-    //     setListaTarefas(novasTarefas);
-    // }
+    const [descricaoEditada, setDescricaoEditada] = useState('');
+    const [editando, setEditando] = useState(false);
+    const [tarefaEditandoIndex, setTarefaEditandoIndex] = useState(null);
+    const descricaoTarefaInputRef = useRef();
 
-    function atualizarTarefa(tarefaAtual){
+    useEffect(() => {
+        const storedTasks = localStorage.getItem("tarefas");
+        if (storedTasks) {
+            setListaTarefas(JSON.parse(storedTasks));
+        }
+    }, []);
+
+    function adicionarTarefa() {
+        const novaTarefa = {
+            descricao: descricaoTarefaInputRef.current.value,
+            finalizado: false
+        };
+
+        const novaListaTarefas = [...listaTarefas, novaTarefa];
+        setListaTarefas(novaListaTarefas);
+        localStorage.setItem("tarefas", JSON.stringify(novaListaTarefas));
+
+        descricaoTarefaInputRef.current.value = '';
+    }
+
+    function removerTarefa(index) {
+        const novaListaTarefas = [...listaTarefas];
+        novaListaTarefas.splice(index, 1);
+        setListaTarefas(novaListaTarefas);
+        localStorage.setItem("tarefas", JSON.stringify(novaListaTarefas));
+    }
+
+    function editarTarefa(index) {
+        setDescricaoEditada(listaTarefas[index].descricao);
+        setEditando(true);
+        setTarefaEditandoIndex(index);
+        descricaoTarefaInputRef.current.value = listaTarefas[index].descricao;
+    }
+
+    function salvarEdicao() {
+        const novaListaTarefas = [...listaTarefas];
+        novaListaTarefas[tarefaEditandoIndex].descricao = descricaoTarefaInputRef.current.value;
+        setListaTarefas(novaListaTarefas);
+        localStorage.setItem("tarefas", JSON.stringify(novaListaTarefas));
+        setEditando(false);
+        descricaoTarefaInputRef.current.value = '';
+    }
+
+    function atualizarTarefa(tarefaAtual) {
         tarefaAtual.finalizado = !tarefaAtual.finalizado;
-        setListaTarefas(listaTarefas.slice())
-        // if(tarefaAtual.finalizado) {
-        //     tarefaAtual.finalizado = false;
-        // } else {
-        //     tarefaAtual.finalizado = true;
-        // }
+        setListaTarefas([...listaTarefas]);
+        localStorage.setItem("tarefas", JSON.stringify(listaTarefas));
     }
 
     function pegaEstilo(tarefaAtual) {
-        if (tarefaAtual.finalizado) {
-            return 'line-through';
-        }
-        return 'none';
+        return tarefaAtual.finalizado ? 'line-through' : 'none';
     }
 
     return (
         <div>
-            <button onClick={adicionarTarefa}>Cadastrar</button>
+            <input type="text" ref={descricaoTarefaInputRef} />
+            {editando ? (
+                <button onClick={salvarEdicao}>Salvar</button>
+            ) : (
+                <button onClick={adicionarTarefa}>Cadastrar</button>
+            )}
             <br />
             <div>
-                {
-                    listaTarefas.map(tarefaAtual => {
-                        return <div style={
-                            {
-
+                {listaTarefas.map((tarefaAtual, index) => (
+                    <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
+                        <div
+                            style={{
+                                flex: 1,
                                 margin: '10px',
                                 color: 'greenyellow',
                                 backgroundColor: 'gray',
                                 textDecoration: pegaEstilo(tarefaAtual)
-                                //textDecoration: tarefaAtual.finalizado ? 'line-through' : 'none'
-                            }
-                        } onClick= {() => atualizarTarefa(tarefaAtual)}>{tarefaAtual.descricao}</div>
-                    })
-                }
+                            }}
+                            onClick={() => atualizarTarefa(tarefaAtual)}
+                        >
+                            {tarefaAtual.descricao}
+                        </div>
+                        <button onClick={() => removerTarefa(index)}>Remover</button>
+                        <button onClick={() => editarTarefa(index)}>Editar</button>
+                    </div>
+                ))}
             </div>
         </div>
     )
-    
-
-    // return (
-    //     <div>
-    //     <button onClick={adicionarTarefa}>Cadastrar</button>
-    //     <br />
-    //         <div>
-    //             {listaTarefas.map((tarefaAtual, index) => (
-    //                 <div
-    //                     key={index}
-    //                     style={{
-    //                         margin: '10px',
-    //                         color: tarefaAtual.finalizado ? 'dark' : 'greenyellow',
-    //                         backgroundColor: 'gray',
-    //                         textDecoration: tarefaAtual.finalizado ? 'line-through' : 'none'
-    //                             }
-    //                     }onClick={() => marcarComoFinalizado(index)}
-    //                 >{tarefaAtual.descricao}
-    //                 </div>
-    //             ))}
-    //         </div>
-    //     </div>
-    // )
 }
 
 export default Tarefas;
